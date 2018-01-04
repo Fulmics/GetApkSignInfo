ccf051154295c8cd3b644d587a257243d678db79
Andrei Conache <xpirt.dev@gmail.com>
2018-01-04 12:26:05 +0100

    Main.java: happy new year

--- src/com/fulmics/xpirt/Main.java
+++ src/com/fulmics/xpirt/Main.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017, Andrei Conache <conache.and@gmail.com>
+ * Copyright (C) 2017-2018, Andrei Conache <conache.and@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
